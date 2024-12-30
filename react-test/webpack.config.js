d3a9f91993fa1cd692d2a0a0d099450db3a495ca
saugjong0521 <saugjong0521@gmail.com>
2024-12-30 16:06:41 +0900

    v0.51

---
+++ react-test/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: path.resolve(__dirname, "public"),
+    port: 3000,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
